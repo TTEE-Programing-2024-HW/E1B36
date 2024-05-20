49dcd4f63740d37671d26162e5e3eef7d10cc71d
E1B36 <167836400+E1B36@users.noreply.github.com>
2024-05-21 02:09:11 +0800

    Update E1B36_HW3.c

--- E1B36_HW3.c
+++ E1B36_HW3.c
@@ -4,7 +4,6 @@
 #include <conio.h>
 #idefine ROWS 9
 #idefine COLS 9
-
 void clearScreen()
 	{
 		system("cls");
@@ -20,7 +19,6 @@ void gisplaymenu()
  		printf("--------------------------\n");
 	}
 void generateSeats(char seats[ROWS][COLS])
-	srand(time(0));
 	for(int i=0;i<ROWS;i++)
 	{
 		for(int j=0;j<COLS;j++)
@@ -52,7 +50,125 @@ void displaySeats(char seats[ROWS][COLS])
 	printf("按任意鍵返回")
 	getch();
 }
-
+void arrangSeatsforuser(char seats[ROWS][COLS],int nSeats)
+{
+	int found=0
+	for(int i=0;i<ROWS;i++)
+	{
+		for(int j=0;j<COLS;j++)
+		{
+			if(seats[i][j]=='-'&&j+nSeats<=COLS)
+			{
+				int available=1;
+			for(int s=0;s<nSeats;s++)
+			{
+				if(seats[i][j+s]!='-')
+				{
+					available=0;
+					break;
+				}
+			}
+			if(available)
+			{
+				for(int s=0;s<nSeats;s++)
+				{
+					seats[i][j+k]!='@';	
+				}				
+				found=1;
+				break;
+			}	
+		}
+	}
+	if(found) break;
+}
+if(!found&&nSeats==4)
+{
+	for(int i=0;i<ROWS-1;i++)
+	{
+		for(int j=0j<COLS;j++)
+		{
+			if(seats[i][j]=='-'&&seats[i+1][j]=='-')
+			{
+				seats[i][j]='@';
+				seats[i+1][j]='@';
+				if(j+1<COLS&&seats[i][j+1]=='-'&&seats[i+1][j]=='-')
+				{
+					seats[i][j+1]='@';
+					seats[i+1]{j}='-';
+					found=1
+					break;
+				}
+				else
+				{
+					seats[i][j]='-';
+					seats[i+1][j]='-';
+				}
+			}
+		}
+		if(found) break;
+	}
+}
+displaySeats(seats);
+char response;
+printf("對安排滿意嗎?(Y/N):");
+scanf("%c",&response);
+if(response=='Y'){
+	for(int i=0;i<COLS;i++)
+	{
+		for(int j=0;j<COLS;j++)
+		{
+			if(seats[i][j]=="@")
+			{
+				seats[i][j]='*';
+			}
+		}
+	}
+}
+else
+{
+	for(int i=0;i<ROWS;i++)
+	{
+		for(int j=0;j<COLS;j++)
+		{
+			if(seats[i][j]=='@')
+			{
+				seats[i][j]='-';
+			}
+		}
+	}
+}
+}
+void chooseSeatsbyuser(char seats[ROWS][COLS])
+{
+	int row,col;
+	char d;
+	printf("輸入座位位置(ex.1-1,2-2)輸入q停止\n");
+	while(1){
+		printf("輸入座位位置:");
+		if(scanf("%d-%d",&row,&col)!=2)
+		break;
+		if(row<1||ros>ROWS||col<1||col>COLS||seats[row-1][col-1]!='-')
+		{
+			printf("失敗,請再試一次");
+			while((d=getchar())!='\n'&&d!=EOE);
+			continue; 
+		}
+		seats[row-1][col-1]='@';
+	}
+	displaySeats(seats);
+	printf("按任意鍵確認選擇");
+	getch();
+	for(int i=0;i<ROWS;i++)
+	{
+		for(int j=0;j<COLS;j++)
+		{
+			if(seats[i][j]=='@')
+			{
+				seats[i][j]='*';
+			}
+		}
+	}
+}
 int main (void){
 //1.個人圖片 星星 
 	puts(".............................................:::....................................................");
@@ -96,5 +212,42 @@ int main (void){
 		else
 			printf("請重新輸入\n");
 	}
-	]
+char ch;
+	switch(ch){
+		case 'a':
+		system("cls");
+		displaySeats(seats);
+		break;
+		case 'b':
+			system("cls");
+			int nSeats;
+			printf("需要幾個位置(1~4)");
+			scanf("%d",&nSeats);
+			if(nSeats<1||nSeats>4)
+			{
+				printf("無效");
+		 	} 
+		 	else
+		 	{
+		 		arrangeSeatsforuser(seats,nSeats);
+		 	}
+		 	break;
+		case 'c':
+			system("cls")
+			chooseSeatsbyuser(seats);
+			break;
+		case 'd':
+			char confirm;
+			printf("繼續?(Y/N):");
+			scanf("%c",&confirm);
+			if(confirm=='N')
+			{
+				printf("退出中")
+				return 0;
+			}
+		default:
+			printf("再試一次");
+			break;
+	return 0;
+	
 }
